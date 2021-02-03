cb97b897d0a279d2babecae115cf4dc955cd4f39
Xavier <github@tttp.eu>
2021-02-03 10:09:36 +0100

    payment intent added

--- index.js
+++ index.js
@@ -1,63 +1,64 @@
-const http = require('http');
-const url = require('url');
-require('dotenv').config();
+const http = require("http");
+const url = require("url");
+require("dotenv").config();
 
-const stripe = require('stripe')('sk_test_...');
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
-const createSecret = (param) => {
-  return {a:"aaa"};
-}
+const paymentIntent = async (param) => {
+  
+  const r= await stripe.paymentIntents.create({
+    amount: param.amount *100 || 404,
+    currency: (param.currency && param.currency.toLowerCase()) || 'eur',
+  payment_method_types: ['card'],
+});
+  console.log(r);
+  return r;
+};
 
 const httpServer = http.createServer(async (request, response) => {
-
-
-const getBody = async req => {
+  const getBody = async (req) => {
     return new Promise((resolve, reject) => {
-       try {
-           let body = '';
-           req.on('data', chunk => {
-               body += chunk.toString(); // convert Buffer to string
-           });
+      try {
+        let body = "";
+        req.on("data", (chunk) => {
+          body += chunk.toString(); // convert Buffer to string
+        });
 
-           req.on('end', () => {
-               //resolve(parse(body));
-               resolve(body);
-           });
-       }
-       catch (e) {
-           reject(e);
-       }
+        req.on("end", () => {
+          //resolve(parse(body));
+          resolve(body);
+        });
+      } catch (e) {
+        reject(e);
+      }
     });
-}
+  };
 
   const parsedUrl = url.parse(request.url, true);
-  response.setHeader('Content-Type', "application/json");
-  response.writeHead(200);
+  response.setHeader("Content-Type", "application/json");
 
   switch (parsedUrl.path) {
-    case '/stripe/oneoff':
-       const body = await getBody(request);
-       let data = null;
+    case "/stripe/oneoff":
+      let data = null;
+      const body = await getBody(request);
       try {
-        data=JSON.parse(body);  
+        data = JSON.parse(body);
       } catch (e) {
-        return response.end(JSON.stringify({error:true,message:"no-json",received:body}));
+        response.statusCode = 404;
+        return response.end(
+          JSON.stringify({ error: true, message: "no-json", received: body })
+        );
       }
-      console.log(data);
-
-       response.end(JSON.stringify(createSecret(data)));
+      response.end(JSON.stringify(await paymentIntent(data)));
       break;
     default:
-      response.writeHead(404);
-      response.end('404 - File Not Found');
+      response.statusCode = 404;
+      response.end(
+        JSON.stringify({ error: true, message: "404", "path": parsedUrl.path })
+      );
   }
-  console.log(parsedUrl);
-
 });
 
-
 httpServer.listen(process.env.PORT || 8888, () => {
-    console.log(`\x1b[32m%s\x1b[0m`, "server running");
-  });
-
-
+  console.log(`\x1b[32m%s\x1b[0m`, "server running");
+});
